ef2b3b80ca7fd736e43c4e66e6834d85be7ac4c1
Kotaro7750 <7750koutarou@gmail.com>
2022-06-19 11:34:18 +0900

    Add: プラットフォームにwindowsを追加

--- src/execution_platform/target_platform.rs
+++ src/execution_platform/target_platform.rs
@@ -2,3 +2,8 @@
 mod unix;
 #[cfg(target_family = "unix")]
 pub use self::unix::*;
+
+#[cfg(taget_family = "windows")]
+mod windows;
+#[cfg(taget_family = "windows")]
+pub use self::windows::*;
---
+++ src/execution_platform/target_platform/windows.rs
@@ -0,0 +1,57 @@
+use std::path::Path;
+use std::process::Command;
+
+use crate::argument_resolver::ArgumentResolver;
+use crate::command_executor::CommandExecutor;
+use crate::execution_platform::ExecutionPlatform;
+use crate::link_executor::LinkExecutor;
+use crate::schema::CommandConfig;
+
+pub struct WindowsExecutionPlatform;
+
+pub fn new() -> WindowsExecutionPlatform {
+    WindowsExecutionPlatform
+}
+
+impl ArgumentResolver for WindowsExecutionPlatform {}
+
+impl CommandExecutor for WindowsExecutionPlatform {
+    fn construct_command(&self, command_config: &CommandConfig) -> Command {
+        let mut command = Command::new("powershell.exe");
+        command.args(["-Command", "Start-Process"]);
+
+        command.arg(self.resolve_argument(&command_config.command));
+
+        command.arg("-ArgumentList");
+        if let Some(ref args) = command_config.args {
+            command.args(args.iter().map(|arg| self.resolve_argument(arg)));
+        }
+
+        command.arg("-Wait");
+
+        if let Some(true) = command_config.as_root {
+            command.arg("-Verb");
+            command.arg("RunAs");
+        }
+
+        command
+    }
+}
+
+impl LinkExecutor for WindowsExecutionPlatform {
+    fn create_link(&self, original: &Path, link: &Path) -> Result<(), Box<dyn std::error::Error>> {
+        let mut command = Command::new("powershell.exe");
+        command.args(["-Command","Start-Process","mklink.exe","-ArgumentList"]);
+
+        if original.is_dir() {
+            command.arg("/d");
+        }
+
+        command.args([original,link]);
+        command.args(["-Wait","-Verb","RunAs"]);
+
+        Ok(())
+    }
+}
+
+impl ExecutionPlatform for WindowsExecutionPlatform {}
