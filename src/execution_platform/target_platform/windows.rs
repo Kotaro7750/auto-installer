c5d1f42904ab8b7046a2e6bc3044ede052343c43
Kotaro7750 <7750koutarou@gmail.com>
2022-06-21 21:37:23 +0900

    fix: windowsでのシンボリックリンク作成コマンドを変更

--- src/execution_platform/target_platform/windows.rs
+++ src/execution_platform/target_platform/windows.rs
@@ -1,12 +1,13 @@
+use std::ffi::OsString;
 use std::path::Path;
 use std::process::Command;
 
 use crate::argument_resolver::ArgumentResolver;
 use crate::command_executor::CommandExecutor;
 use crate::execution_platform::ExecutionPlatform;
-use crate::ExecutionError;
 use crate::link_executor::LinkExecutor;
 use crate::schema::CommandConfig;
+use crate::ExecutionError;
 
 pub struct WindowsExecutionPlatform;
 
@@ -42,14 +43,16 @@ impl CommandExecutor for WindowsExecutionPlatform {
 impl LinkExecutor for WindowsExecutionPlatform {
     fn create_link(&self, original: &Path, link: &Path) -> Result<(), Box<dyn std::error::Error>> {
         let mut command = Command::new("powershell.exe");
-        command.args(["-Command", "Start-Process", "mklink.exe", "-ArgumentList"]);
+        command.arg("-Command");
 
-        if original.is_dir() {
-            command.arg("/d");
-        }
+        let mut inner_command  = OsString::from("Start-Process powershell.exe -ArgumentList '-Command','New-Item -ItemType SymbolicLink -Path ");
+        inner_command.push(format!("\"{}\"", original.display()));
+        inner_command.push(" -Value ");
+        inner_command.push(format!("\"{}\"", link.display()));
+        inner_command.push("'");
 
-        command.args([link, original]);
-        command.args(["-Wait", "-Verb", "RunAs"]);
+        command.arg(inner_command);
+        command.args(["-Wait", "-Verb", "RusAs"]);
 
         println!("execute `{:?}`", command);
 
